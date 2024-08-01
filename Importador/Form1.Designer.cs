91ef6ab6cf0554817c18f8b8ede3ea34ae11b978
Alexandre Pereira <alexandrepereirax643@gmail.com>
2024-07-31 23:45:51 -0300

    alteração botão 3

--- Importador/Form1.Designer.cs
+++ Importador/Form1.Designer.cs
@@ -28,13 +28,74 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(55, 243);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(207, 160);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "teste carai";
+            this.button1.UseMnemonic = false;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(55, 35);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(207, 160);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "teste carai";
+            this.button2.UseMnemonic = false;
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(404, 35);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(207, 160);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "teste carai";
+            this.button3.UseMnemonic = false;
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(404, 243);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(207, 160);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "teste carai";
+            this.button4.UseMnemonic = false;
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
 
